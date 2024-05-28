dc4e10d5eb43aaa5023a804aee6911ebaaf35401
pinknightty <rafagrosa@gmail.com>
2024-05-28 19:16:21 -0300

    Acho que agora vai o fork correto

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,53 @@
+import { FormEvent, useState } from 'react'
+
+import styled from 'styled-components'
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: var(--cor-secundaria);
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+`
+
+const Campo = styled.input`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+}
+`
+const BotaoPesquisar = styled.button`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;
+`
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </Form>
+  )
+}
+export default FormVagas
